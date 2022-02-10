da1aacd041e0414ce660a3cebc1e6a3f3edba339
lexagonal <16949155+lexagonal@users.noreply.github.com>
2022-02-10 06:52:07 -0800

    Add title, imageview, button, and change imageview on click.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    let eightBallImages = [
+        UIImage(named: "ball1"),
+        UIImage(named: "ball2"),
+        UIImage(named: "ball3"),
+        UIImage(named: "ball4"),
+        UIImage(named: "ball5"),
+    ]
+    
+    @IBOutlet weak var eightBallImageVIew: UIImageView!
 
-
-
+    @IBAction func askClicked(_ sender: Any) {
+        eightBallImageVIew.image = eightBallImages[Int.random(in: 0...4)]
+    }
 }
 
